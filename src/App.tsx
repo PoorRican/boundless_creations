fc9a090a446bc3038211a02539a9e58b83157048
Josue Figueroa <poor.rican@pm.me>
2023-06-30 12:49:33 -0700

    Create placeholder content component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Heading from "./components/Heading";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
         <Heading />
+        <Home />
     </div>
   );
 }
