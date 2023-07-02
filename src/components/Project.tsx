b8070808053d815273634d38e756e205d34a614a
Josue Figueroa <poor.rican@pm.me>
2023-07-01 17:45:02 -0700

    Represent `Project` as card

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,28 +4,46 @@ export enum Status {
     Active
 }
 
+const StatusBadge = (props: {status: Status}) => {
+  const [style, text] = function() {
+    switch (props.status) {
+      case Status.InProgress:
+        return ['bg-red-50 text-red-700 ring-red-600/10', 'In Progress']
+      case Status.AsIs:
+        return ['bg-orange-50 text-orange-700 ring-orange-600/10', 'As Is']
+      case Status.Active:
+        return ['bg-green-50 text-green-700 ring-green-600/10', 'Active']
+    }
+  }();
+  return (
+    <span className={`${style} mb-4 text-white items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset`}
+          style={{fontWeight: 100}}
+          children={text}
+    />
+  )
+}
+
 interface ProjectIface {
     title: string;
     description: string;
     status: Status;
+    url?: string;
     key?: number;
 }
 
 export const Project = (props: ProjectIface) => {
     return (
-        <article className="m-4 h-40 bg-slate-900 ring-2 p-5 rounded-2xl">
-            <h1 className="text-xl">
-                {props.title}
+      <a href={props.url ? props.url : "#"}>
+        <article className="m-4 h-40 bg-slate-100 p-5 rounded-md">
+          <h3 className="text-xl text-slate-900 mr-2.5">
+            {props.title}
+          </h3>
 
-            </h1>
+          <StatusBadge status={props.status} />
 
-            <span className="px-1 py-0.5 ml-2.5 text-xs rounded"
-                  style={{backgroundColor: "red"}}
-                  children={props.status}
-            />
-
-            <p className="mt-2 overflow-ellipsis"
-               children={props.description} />
+          <p className="mt-2 overflow-ellipsis text-sm text-slate-800" style={{fontWeight: 200}}
+             children={props.description} />
         </article>
+      </a>
     )
 }
