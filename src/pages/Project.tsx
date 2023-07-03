6b65e0000a1b413b3f1beb858f4cc37c28cefe5a
Josue Figueroa <poor.rican@pm.me>
2023-07-03 16:03:31 -0700

    Add styling to markdown elements for project description

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -3,6 +3,7 @@ import {StatusBadge} from "../components/StatusBadge";
 import {useLoaderData} from "react-router-dom";
 import {ProjectLinkIface} from "../components/ProjectCard";
 import ReactMarkdown from "react-markdown";
+import React from "react";
 
 const ProjectLink = (props: ProjectLinkIface) => {
   return (
@@ -43,7 +44,17 @@ const Description = (props: {description: string}) => {
         <h2 className={"text-lg mb-2 font-light"}>
           Description
         </h2>
-        <ReactMarkdown className={"font-thin"}>
+        <ReactMarkdown className={"font-thin"}
+                       components={{
+                         p: ({node, ...props}) => <p className="text-base my-1 font-thin" {...props} />,
+                         // eslint-disable-next-line jsx-a11y/heading-has-content
+                         "h1": ({node, ...props}) => <h1 className="text-2xl my-2 font-thin" {...props} />,
+                         // eslint-disable-next-line jsx-a11y/heading-has-content
+                         "h2": ({node, ...props}) => <h2 className="text-xl my-2 font-thin" {...props} />,
+                         // eslint-disable-next-line jsx-a11y/heading-has-content
+                         "h3": ({node, ...props}) => <h3 className="text-lg my-2 font-light" {...props} />,
+                       }}
+        >
           {props.description}
         </ReactMarkdown>
       </div>
