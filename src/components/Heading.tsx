c6f51dd6ac5f80da04ed9e5342298eed4b135cc7
Josue Figueroa <poor.rican@pm.me>
2023-11-28 01:42:05 -0500

    Update `Heading`

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,3 @@
-import { Disclosure } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { NavLink } from "react-router-dom";
 
 const Heading = () => {
@@ -15,81 +13,44 @@ const Heading = () => {
   ];
 
     return (
-      <div className="min-h-full">
-        <Disclosure as="nav" className="bg-indigo-800">
-          {({ open }) => (
-            <>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="flex h-16 items-center justify-between">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <div className={"leading-none font-mono text-xs subpixel-antialiased text-yellow-400 whitespace-pre "}>
-                        <p>{`┏━┓┳━┓┳━┓┳━┓┏┓┓o┏━┓┏┓┓`}</p>
-                        <p>{`┃  ┃┳┛┣━ ┃━┫ ┃ ┃┃ ┃┃┃┃`}</p>
-                        <p>{`┗━┛┇┗┛┻━┛┛ ┇ ┇ ┇┛━┛┇┗┛`}</p>
-                      </div>
-                    </div>
-                    <div className="hidden md:block">
-                      <div className="ml-10 flex items-baseline space-x-4">
-                        {routes.map(({path, name}) => {
-                          return (
-                            <NavLink
-                              key={name}
-                              to={path}
-                              className={
-                                ({isActive}) => {
-                                  const style = " rounded-md px-3 py-2 text-sm font-medium"
-                                  return isActive ? 'bg-blue-800 text-white' + style
-                                    : 'text-gray-300 hover:bg-gray-700 hover:text-white' + style
-                                }
-                              }
-                            >
-                              {name}
-                            </NavLink>
-                          );
-                        })}
-                      </div>
-                    </div>
-                  </div>
-                  <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                      )}
-                    </Disclosure.Button>
-                  </div>
-                </div>
-              </div>
+      <div className="min-h-full bg-indigo-800">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex flex-col items-center justify-center">
 
-              <Disclosure.Panel className="md:hidden">
-                <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                  {routes.map(({name, path}) => (
-                    <Disclosure.Button
+            {/* title */}
+            <div className={"mt-8"}>
+              <div className={"leading-none font-mono text-xs md:leading-none md:text-lg subpixel-antialiased text-yellow-400 whitespace-pre "}>
+                <p>{`┏━┓┳━┓┳━┓┳━┓┏┓┓o┏━┓┏┓┓`}</p>
+                <p>{`┃  ┃┳┛┣━ ┃━┫ ┃ ┃┃ ┃┃┃┃`}</p>
+                <p>{`┗━┛┇┗┛┻━┛┛ ┇ ┇ ┇┛━┛┇┗┛`}</p>
+              </div>
+            </div>
+
+            {/* links */}
+            <div>
+              <div className="my-6 flex items-baseline space-x-4">
+                {routes.map(({path, name}) => {
+                  return (
+                    <NavLink
                       key={name}
-                    >
-                      <NavLink
-                        to={path}
-                        className={
-                          ({isActive}) => {
-                            const style = " rounded-md px-3 py-2 text-sm font-medium"
-                            return isActive ? 'bg-blue-800 text-white' + style
-                              : 'text-gray-300 hover:bg-gray-700 hover:text-white' + style
-                          }
+                      to={path}
+                      className={
+                        ({isActive}) => {
+                          const style = " px-3 py-2 text-sm font-medium"
+                          return isActive ? 'underline underline-offset-2 text-white' + style
+                            : 'text-gray-300 hover:bg-indigo-700 hover:text-white' + style
                         }
-                      >
-                        {name}
-                      </NavLink>
-                    </Disclosure.Button>
-                  ))}
-                </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
+                      }
+                    >
+                      {name}
+                    </NavLink>
+                  );
+                })}
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
     )
 }
