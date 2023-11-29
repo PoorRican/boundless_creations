f38383685fa8ec2db6757137b04963b0397c8920
Josue Figueroa <poor.rican@pm.me>
2023-11-29 18:34:16 -0500

    Remove `InterestsSection` from home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -22,7 +22,6 @@ const Home = () => {
 
         <div className={"flex flex-col gap-8"}>
           <ContactSection className="ring-1 h-40 p-6 rounded-xl" />
-          <InterestsSection className={"ring-1 rounded-xl p-6"}/>
         </div>
 
         <div className="md:col-start-1 md:col-end-3">
