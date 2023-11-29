db9296d55b675148e31d342193a9daf5e8740851
Josue Figueroa <poor.rican@pm.me>
2023-11-29 17:12:09 -0500

    Deprecate `add_project_images()`

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -2,7 +2,6 @@ import {useEffect} from "react";
 import projects from "../../assets/projects.json"
 import ProjectSection from "../../components/ProjectSection";
 import ContentWrapper from "../../components/ContentWrapper";
-import {add_project_images} from "../../helpers";
 
 
 /**
@@ -18,7 +17,7 @@ const Projects = () => {
     <ContentWrapper>
       <h1 className={"mb-12 text-4xl font-thin"}>Projects</h1>
 
-      <ProjectSection projects={add_project_images(projects)} className={""} />
+      <ProjectSection projects={projects} className={""} />
     </ContentWrapper>
   )
 }
